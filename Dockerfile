8a5588254a0598edf89a36abcd9a8e2643204739
TheLaneagen <devinjyork@gmail.com>
2025-12-19 13:31:47 -0600

    Finished goserver assignment

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,7 @@
-# Use a lightweight Debian base image
 FROM debian:stable-slim
 
-# Copy your compiled Go server into the container
 COPY goserver /bin/goserver
 
-# Make the binary executable
-RUN chmod +x /bin/goserver
+EXPOSE 8991
 
-ENV PORT=8991
-
-# Start the server when the container runs
 CMD ["/bin/goserver"]
--- main.go
+++ main.go
@@ -14,6 +14,9 @@ func main() {
 	m.HandleFunc("/", handlePage)
 
 	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT not set")
+	}
 	srv := http.Server{
 		Handler:      m,
 		Addr:         ":" + port,
@@ -34,7 +37,7 @@ func handlePage(w http.ResponseWriter, r *http.Request) {
 	const page = `<html>
 <head></head>
 <body>
-	<p> Hello from Docker! I'm a Go server. </p>
+	<p> Hi Docker, I pushed a new version. </p>
 </body>
 </html>
 `
